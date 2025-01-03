e1a5bab96649cfcbadaa7303b96f27930ca33dd7
hsusana <152581281+hsusana20@users.noreply.github.com>
2025-01-03 08:35:20 +0000

    add a file for office staff for requirements

---
+++ AshenKeep/resources/views/officestaff_requirements.blade.php
@@ -0,0 +1,16 @@
+<x-app-layout>
+
+    <div class="flex py-12">
+        <!-- Sidebar -->
+        <div class="hidden sm:flex">
+            <x-dashboard-side-bar />
+        </div>
+
+        <!-- Main Content -->
+        <div class="flex-1">
+            <div class="py-1">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-6">
+                        <h1 class="text-2xl font-bold mb-6">Hi, Office Staff!</h1>
+    </div>
+</x-app-layout>
