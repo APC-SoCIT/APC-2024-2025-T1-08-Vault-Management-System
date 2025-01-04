37ddc195d631f2a707f02db97627fd1a2b300d23
heiro27 <heirousana27@gmail.com>
2025-01-04 21:45:27 +0800

    add admin requirements file

---
+++ AshenKeep/resources/views/admin_requirements.blade.php
@@ -0,0 +1,100 @@
+<x-app-layout>
+    <div class="flex py-12">
+        <!-- Sidebar -->
+        <div class="hidden sm:flex">
+            <x-dashboard-side-bar />
+        </div>
+
+        <!-- Main Content -->
+        <div class="max-h-[600px] overflow-auto border border-black bg-blue-900 text-white rounded-lg p-6 w-full mx-6">
+            <h3 class="text-2xl font-semibold mb-6 text-white">Review Requirements</h3>
+            <table class="table-fixed w-full divide-y divide-gray-200 text-center border-collapse border-separate border-spacing-y-2 rounded-md overflow-hidden">
+                <thead class="bg-blue-900 w-full">
+                    <tr>
+                        <th class="p-6">ID</th>
+                        <th class="p-6">Name</th>
+                        <th class="p-6">Type</th>
+                        <th class="p-6">Format</th>
+                        <th class="p-6">Date</th>
+                        <th class="p-6">Time</th>
+                        <th class="p-6">Status</th>
+                    </tr>
+                </thead>
+                <tbody class="auto-rows-auto">
+                    <tr class="bg-white text-black">
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                    </tr>
+                    <tr class="bg-white text-black">
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                    </tr>
+                    <tr class="bg-white text-black">
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                    </tr>
+                    <tr class="bg-white text-black">
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                    </tr>
+                    <tr class="bg-white text-black">
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                    </tr>
+                    <tr class="bg-white text-black">
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                    </tr>
+                    <tr class="bg-white text-black">
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                    </tr>
+                    <tr class="bg-white text-black">
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</x-app-layout>
