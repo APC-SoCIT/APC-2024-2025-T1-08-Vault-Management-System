6a26ef53a96ef1e864bb08d539043647d37bb14d
kppantino <kppantino@student.apc.edu.ph>
2025-01-04 13:57:21 +0000

    Vaults update

--- AshenKeep/resources/views/vault.blade.php
+++ AshenKeep/resources/views/vault.blade.php
@@ -19,46 +19,112 @@
                
                 <!-- Vault List Box -->
                 <div class="bg-blue-900 rounded-lg p-6">
-                    <div class="flex justify-between items-center mb-4"></div>
+                    <div class="flex justify-between items-center mb-4">
                         <h2 class="text-white text-xl font-semibold">Vaults List</h2>
-                        <div class="flex items-center space-x-4 ml-auto"></div>
-                            <select id="locationFilter" class="form-select bg-blue-700 text-white">
+                        <div class="flex items-center space-x-4 ml-auto">
+                            <select id="locationFilter" class="form-select bg-blue-900 text-white rounded-lg border border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-300" >
                                 <option value="All">All</option>
                                 <option value="Inner Southwest">Inner Southwest</option>
                                 <option value="Outer Northwest">Outer Northwest</option>
                                 <option value="Inner South">Inner South</option>
                                 <option value="Outer North">Outer North</option>
                             </select>
-                            <button class="btn bg-green-500 text-white hover:bg-green-600" id="addRowButton">Add New Vault</button>
+                            <button class="btn bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300">
+                                Add New Vault
+                            </button>
                         </div>
                     </div>
- 
-                    <!-- Vault List Table with White Background -->
-                    <div class="bg-white rounded-lg p-6">
+
+
                     <div class="overflow-auto max-h-[400px]">
-                        <table class="table table-bordered text-gray-800 w-full">
-                        <thead class="table-dark">
-                            <tr>
-                            <th>Vault Number</th>
-                            <th>Location</th>
-                            <th>Vault Owner ID</th>
-                            <th>Owner</th>
-                            <th>Date Issued</th>
-                            <th>Urns Quantity</th>
-                            <th>Availability</th>
-                            <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody id="vaultTableBody">
-                            <!-- New Vault Information Appears Here -->
-                        </tbody>
+                        <table class="table table-bordered w-full">
+                            <thead class="bg-blue-900 text-white text-lg font-semibold">
+                                <tr>
+                                    <th class="p-2">Vault Number</th>
+                                    <th class="p-2">Location</th>
+                                    <th class="p-2">Vault Owner ID</th>
+                                    <th class="p-2">Owner</th>
+                                    <th class="p-2">Date Issued</th>
+                                    <th class="p-2">Urns Quantity</th>
+                                    <th class="p-2">Availability</th>
+                                    <th class="p-2">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white text-blue-900 shadow-lg rounded-xl p-6 mt-6">
+                                <tr>
+                                    <th class="p-2">hi</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                </tr>
+
+                                <tr>
+                                    <th class="p-2">hi</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                </tr>
+
+                                <tr>
+                                    <th class="p-2">hi</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                </tr>
+
+                                <tr>
+                                    <th class="p-2">hi</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                </tr>
+
+                                <tr>
+                                    <th class="p-2">hi</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                </tr>
+
+                                <tr>
+                                    <th class="p-2">hi</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                    <th class="p-2">sample</th>
+                                </tr>
+
+                                
+                            </tbody>
                         </table>
                     </div>
-                    </div>
-                </div>
                 </div>
             </div>
             </div>
         </div>
     </div>
+
 </x-app-layout>
