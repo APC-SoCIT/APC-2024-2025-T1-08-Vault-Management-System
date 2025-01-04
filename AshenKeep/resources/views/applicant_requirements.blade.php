0b236b9890c3c417f807e689b6d298f64c5c49e3
heiro27 <heirousana27@gmail.com>
2025-01-04 21:37:36 +0800

    fix the table for applicant requirements

--- AshenKeep/resources/views/applicant_requirements.blade.php
+++ AshenKeep/resources/views/applicant_requirements.blade.php
@@ -11,18 +11,18 @@
             <x-dashboard-side-bar />
         </div>
         <!-- Main Content -->
-         <div class="max-h-96 overflow-auto border border-black bg-blue-900 text-white rounded-lg p-6 w-3/4 ml-auto mr-[100px]">
+        <div class="max-h-[600px] overflow-auto border border-black bg-blue-900 text-white rounded-lg p-6 w-full mx-6">
             <h3 class="text-2xl font-semibold mb-6 text-white">Requirements</h3>
             <table class="table-fixed w-full divide-y divide-gray-200 text-center border-collapse border-separate border-spacing-y-2 rounded-md overflow-hidden">
-                <thead class="bg-blue-900">
+                <thead class="bg-blue-900 w-full">
                     <tr>
-                        <th class="p-3">ID</th>
-                        <th class="p-3">Name</th>
-                        <th class="p-3">Type</th>
-                        <th class="p-3">Format</th>
-                        <th class="p-3">Date</th>
-                        <th class="p-3">Time</th>
-                        <th class="p-3">Status</th>
+                        <th class="p-6">ID</th>
+                        <th class="p-6">Name</th>
+                        <th class="p-6">Type</th>
+                        <th class="p-6">Format</th>
+                        <th class="p-6">Date</th>
+                        <th class="p-6">Time</th>
+                        <th class="p-6">Status</th>
                     </tr>
                 </thead>
                 <tbody class="auto-rows-auto">
@@ -88,7 +88,16 @@
                         <td class="p-6"></td>
                         <td class="p-6"></td>
                         <td class="p-6"></td>
-                    </tr>  
+                    </tr>
+                    <tr class="bg-white text-black">
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                        <td class="p-6"></td>
+                    </tr>
                 </tbody>
             </table>
         </div>
