9bb437e8ce8a385891779f302845f7bfc4d4903e
John Emmanuel Piala <71298426+jbpiala@users.noreply.github.com>
2025-01-11 08:57:36 +0000

    Deleted duplicates, Added Edit and Save Button Components, Made Applicant Vault Controller and Model

---
+++ AshenKeep/app/Http/Controllers/ApplicantVaultController.php
@@ -0,0 +1,17 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\ApplicantVault;
+ 
+class ApplicantVaultController extends Controller
+{
+    
+    public function index()
+    {
+        $applicantVaults=ApplicantVault::all();
+        return view('YUNG VIEW', compact('applicantVaults'));
+    }
+}
