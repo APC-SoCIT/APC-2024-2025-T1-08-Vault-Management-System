027a3e1c4d0fb24e7386fb18c7c8f177d6eef392
heiro27 <heirousana27@gmail.com>
2025-02-08 19:54:32 +0800

    update file

--- AshenKeep/routes/web.php
+++ AshenKeep/routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ApplicantController;
+use App\Http\Controllers\RequirementController;
+
 
 $url = config('app.url');
 URL::forceRootUrl($url);
