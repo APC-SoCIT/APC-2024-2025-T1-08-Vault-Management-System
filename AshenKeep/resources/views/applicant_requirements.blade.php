71909513db176bee691d5108e795dc7e24a4de91
heiro27 <heirousana27@gmail.com>
2025-02-09 18:00:28 +0800

    remove sidebar

--- AshenKeep/resources/views/applicant_requirements.blade.php
+++ AshenKeep/resources/views/applicant_requirements.blade.php
@@ -1,10 +1,5 @@
 <x-app-layout>
-    <div class="flex">
-        <div class="hidden sm:flex">
-            <x-dashboard-side-bar />
-        </div>
-        <div class="flex-1">
-            <livewire:applicant-requirements />
-        </div>
+    <div class="flex-1">
+        <livewire:applicant-requirements />
     </div>
 </x-app-layout>
