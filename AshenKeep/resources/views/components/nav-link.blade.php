782e54adc893658f70cef13e9581d2876bab6094
John Emmanuel Piala <71298426+jbpiala@users.noreply.github.com>
2025-02-20 14:55:12 +0000

    Added manage application, manage requirements and edited nav-link file to include hover effect on text

--- AshenKeep/resources/views/components/nav-link.blade.php
+++ AshenKeep/resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center p-10 font-semibold px-1 pt-1 rounded border-transparent text-xl leading-5 text-black dark:text-gray-400 hover:bg-keep-white focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center p-10 font-semibold px-1 pt-1 rounded border-transparent text-xl leading-5 text-black dark:text-gray-400 hover:bg-keep-white focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out';
+    ? 'inline-flex items-center px-8 py-4 font-semibold rounded border-transparent text-xl leading-5 text-black dark:text-gray-400 hover:text-white focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out'
+    : 'inline-flex items-center px-8 py-4 font-semibold rounded border-transparent text-xl leading-5 text-black dark:text-gray-400 hover:text-white focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
